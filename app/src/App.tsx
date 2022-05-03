a140f68e31f06d650c9800336f45e602680be6ce
top1st <riguang102@gmail.com>
2022-05-03 10:15:51 +0800

    update article account

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Paper, Skeleton, TextField, Button } from "@mui/material";
 import "./App.css";
 import WalletContext from "./WalletContext";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
-import { AnchorProvider, Program } from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js"
-import idl from './solana_global_article.json'
+import { AnchorProvider, Program, web3 } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { IDL } from "./solana_global_article";
+import config from "./config";
 
+const programID = new PublicKey("zaR6MXmj1DjeCdNBix6R6CA53H6rRXRoiBh4LEAvP2G");
+
+const { SystemProgram, Keypair } = web3;
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
@@ -32,7 +36,57 @@ const App = () => {
   const initialize = async () => {
     if (!wallet) return;
     const provider = new AnchorProvider(connection, wallet, {});
+    const program = new Program(IDL, programID, provider);
+    const keyPairOne = Keypair.generate();
+    try {
+      await program.methods
+        .initialize()
+        .accounts({
+          article: keyPairOne.publicKey,
+          personThatPays: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([keyPairOne])
+        .rpc();
+      console.log("done", keyPairOne.publicKey.toString());
+    } catch (error) {
+      console.log("#1", error);
+      return alert(error);
+    }
   };
+
+  const uploadWords = async () => {
+    if (!wallet) return;
+    const provider = new AnchorProvider(connection, wallet, {});
+    const program = new Program(IDL, programID, provider);
+    try {
+      await program.methods
+        .writeIntoArticle(inputValue)
+        .accounts({ article: config.solana_article_account })
+        .rpc();
+    } catch (error) {
+      console.log("#2", error);
+      return alert(error);
+    }
+    getAndSetArticle();
+  };
+
+  const getAndSetArticle = useCallback(async () => {
+    if (!wallet) return;
+    const provider = new AnchorProvider(connection, wallet, {});
+    const program = new Program(IDL, programID, provider);
+    const aritcleData = await program.account.article.fetch(
+      config.solana_article_account
+    );
+    setSolanaArticle(aritcleData.content);
+    setIsLoading(false);
+  }, [connection, wallet]);
+
+  useEffect(() => {
+    if (wallet) {
+      getAndSetArticle();
+    }
+  }, [getAndSetArticle, wallet]);
 
   return (
     <>
@@ -46,11 +100,17 @@ const App = () => {
           <WalletMultiButton />
         </div>
       </header>
-      <Paper elevation={20} className="content-box">
-        <Skeleton variant="text" />
-        <Skeleton variant="text" />
-        <Skeleton variant="text" />
-      </Paper>
+      {isLoading ? (
+        <Paper elevation={20} className="content-box">
+          <Skeleton variant="text" />
+          <Skeleton variant="text" />
+          <Skeleton variant="text" />
+        </Paper>
+      ) : (
+        <Paper elevation={20} className="content-box">
+          {solanaArticle}
+        </Paper>
+      )}
       <div className="three-words-input-container">
         <TextField
           id="outlined-basic"
@@ -60,9 +120,14 @@ const App = () => {
           value={inputValue}
           onChange={checkAndAddWords}
         />
-        <Button variant="contained" className="submit-button">
+        <Button
+          variant="contained"
+          className="submit-button"
+          onClick={uploadWords}
+        >
           Submit
         </Button>
+        {/* <Button onClick={initialize} color="secondary" variant="contained">initialize</Button> */}
       </div>
     </>
   );
--- app/src/config.ts
+++ app/src/config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    solana_article_account: '',
+    solana_article_account: '6QMmCpu1RC8LNmQq1zHwT9vKq22nESc9cjcDuDsgDftd',
     network: 'devnet',
     endpoint: 'https://api.devnet.solana.com',
 }
