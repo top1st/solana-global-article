11de68d8857ceaeb017f064e28a295edb8adf4b0
top1st <riguang102@gmail.com>
2022-05-03 00:45:07 +0000

    update key

--- programs/solana-global-article/src/lib.rs
+++ programs/solana-global-article/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("zaR6MXmj1DjeCdNBix6R6CA53H6rRXRoiBh4LEAvP2G");
 
 #[program]
 pub mod solana_global_article {
